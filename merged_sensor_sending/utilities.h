6cf11c49d10724682d5a0f1aee7405b84dbdd7ae
andrgram <54682731+andrgram@users.noreply.github.com>
2020-02-13 15:46:21 +0100

    Update utilities.h

---
+++ merged_sensor_sending/utilities.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <WiFi.h>
+#include <FirebaseESP32.h>
+#include "time.h"
+
+// Global defines
+#define WIFI_SSID "networkTestAndreas"
+#define WIFI_PASSWORD "123456789"
+#define FIREBASE_HOST "radbird-elsys.firebaseio.com" //Do not include https:// in FIREBASE_HOST
+#define FIREBASE_AUTH "EWydgguCOq7ZUTcxpdHtaxLoA8NC5Z3PyLwlcs0q"
+
+
+void connectWiFi();
+
+String getTimestamp();
